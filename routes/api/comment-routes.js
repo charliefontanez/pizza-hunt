cd13eec401e0867bc37f147a76bda15a21b1db53
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-20 15:44:25 -0400

    fixing reply model

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getAllComments, addComment, removeComment } = require('../../controllers/comment-controller');
+const { getAllComments, addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 router.route('/:pizzaId').post(addComment);
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 router.route('/').get(getAllComments);
 
 
